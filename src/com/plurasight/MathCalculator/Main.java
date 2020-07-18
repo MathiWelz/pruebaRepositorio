7c146568d175c06da7bd07d7c773e03df699c856
Mathias <welzmathias@gmail.com>
2020-07-17 23:46:48 -0300

    Added sout Hello World

--- src/com/plurasight/MathCalculator/Main.java
+++ src/com/plurasight/MathCalculator/Main.java
@@ -3,6 +3,6 @@ package com.plurasight.MathCalculator;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello Mundo!");
     }
 }
